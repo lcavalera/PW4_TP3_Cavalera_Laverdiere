23e227a091f5a22a550c2e164d4c56dfb2c9d01f
Luca H23 Dev Info <2298949@cegeplimoilou.ca>
2024-05-19 13:51:45 -0400

    ajout page participer

--- src/web4.ui/src/components/EvenementParticiper.vue
+++ src/web4.ui/src/components/EvenementParticiper.vue
@@ -1,81 +1,60 @@
 <template>
-    <div>
-    <label>Titre: </label><input id="titre" type="text" v-model="todo.text" :disabled="!isEdit">
-    </div><br>
-    <div>
-    <label>Completé: </label><input id="status" type="checkbox" v-model="todo.done" :disabled="!isEdit">
-    </div><br>
-    <div>
-    <label>Categorie: </label>
-    <select name="categories" id="categorie" v-model="todo.category" :disabled="!isEdit">
-      <option v-for="(item, index) in categories" :key="index" :value="item">{{ item }}</option>
-    </select>
-    </div>
-    </template>
-    
-    <script>
-    import { mapState, mapActions } from 'vuex'
-    import httpclient from '@/api/httpclient'
-    
-    export default {
-      name: "TodoForm",
-      props: {
-        titre: String,
-      },
-      data() {
-        return {
-          todo: {},
-          isEdit: false
-        };
-      },
-      methods: {
-        ...mapActions({ getData: 'getData'}),
-        getTodoApi(id){
-          httpclient.get(`Todos/${id}`)
-          .then(reponse => this.todo = reponse.data)
-          .catch(error => console.log(error))
-        },
-        getCategorieApi(){
-          httpclient.get(`categorie`)
-          .then(reponse => this.categorie = reponse.data)
-          .catch(error => console.log(error))
-        }
-      },
-      created(){
-        this.isEdit = this.$route.params.action === 'edit'
-        this.getTodoApi(this.$route.params.id)
-        this.getData()
-      },
-      computed: {
-        ...mapState({ categories: 'categories'})
+  <form class="evenement">
+  <label>Titre: </label><input id="titre" type="text" v-model="evenement.titre">
+  <label>Date Debut: </label><input id="titre" type="text" v-model="evenement.dateDebut">
+  <label>Date Fin: </label><input id="titre" type="text" v-model="evenement.dateDeFin">
+  <label>Desctiption: </label><textarea id="titre" type="text" v-model="evenement.description"></textarea>
+  <label>Adresse: </label><input id="titre" type="text" v-model="evenement.adresse">
+  <label>Nom Organisateur: </label><input id="titre" type="text" v-model="evenement.nomOrganisateur">
+  <label>Prix: </label><input id="titre" type="text" v-model="evenement.prix">
+  <!-- <label>Categories: </label><input v-for="(item, index) in evenement.categories" :key="index" id="titre" type="text" :value="item"> -->
+  <label>Ville: </label><input id="titre" type="text" v-model="evenement.villeNom">
+  <label>Ids participations: </label><input id="titre" type="text" v-model="evenement.participationIds">
+</form>
+<button>Soumettre</button>
+</template>
+  
+  <script>
+  import httpclient from '@/api/httpclient'
+  
+  export default {
+    name: "EvenementParticipation",
+    props: {
+      titre: String,
+    },
+    data() {
+      return {
+        evenement: {}
+      };
+    },
+    methods: {
+      getEvenementsApi(id){
+        httpclient.get(`Evenements/${id}`)
+        .then(reponse => this.evenement = reponse.data)
+        .catch(error => console.log(error))
       }
-    };
-    </script>
-    
-    <style scoped>
-    .done {
-      background: rgb(7, 240, 7);
+    },
+    created(){
+      this.getEvenementsApi(this.$route.params.id)
     }
-    
-    button,
-    [aria-label] {
-      cursor: pointer;
-    }
-    
-    table {
-      font-family: Arial, sans-serif;
-      border: 1px solid;
-      border-collapse: collapse;
-      width: 100%;
-    }
-    
-    th {
-      background-color: #f8f8f8;
-      padding: 5px;
-    }
-    
-    td {
-      border: 1px solid;
-      padding: 5px;
-    }
-    </style>
+  };
+  </script>
+  
+  <style scoped>
+ 
+  button,
+  [aria-label] {
+    cursor: pointer;
+  }
+  .evenement{
+    margin: auto;
+    display: grid;
+    font-family: Arial, sans-serif;
+    width: 500px;
+  }
+  .evenement label{
+    justify-items: center;
+    font-weight: bold;
+    text-align: left;
+  }
+  </style>
